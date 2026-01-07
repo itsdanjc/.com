498304c706dd83814d4485f2a38e486824598d32
itsdanjc <dan@itsdanjc.com>
2026-01-07 20:09:29 +0000

    Removed dep click, using argparse instead.

--- sitegen/__main__.py
+++ sitegen/__main__.py
@@ -1,5 +1,9 @@
-import os, click, logging, time
+import os
+import argparse
+import logging
+import time
 from pathlib import Path
+from typing import Optional
 from jinja2 import TemplateError
 from .log import configure_logging
 from .site import SiteRoot
@@ -10,26 +14,42 @@ from . import __version__
 logger = logging.getLogger(__name__)
 cwd = Path(os.getcwd())
 
-@click.group()
-@click.option('--verbose', '-v', is_flag=True, default=False)
-@click.version_option(version=__version__)
-def cli(verbose: bool) -> None:
-    configure_logging(verbose)
+def main(argv: Optional[list[str]] = None) -> None:
+    parser = argparse.ArgumentParser(prog="cli")
+    commands = parser.add_subparsers(dest="command", required=True)
 
-@cli.command(help="Build the site.")
-@click.option(
-    '--force',
-    help="Build all pages, even if unmodified",
-    is_flag=True,
-    default=False
-)
-@click.option(
-    "--directory",
-    default=cwd,
-    type=Path,
-    help="Use the specified directory, instead of the current directory"
-)
-def build(force: bool, directory: Path):
+    # Global Arguments
+    parser.add_argument("-v", "--verbose", action="store_true", help="Show more info in logs")
+    parser.add_argument("--version", action="version", version=__version__)
+
+    # Build command
+    build_cmd = commands.add_parser("build", help="Build the site.")
+    build_cmd.add_argument(
+        "-f","--force", action="store_true", help="Rebuild all pages.")
+    build_cmd.add_argument(
+        "-c","--clean", action="store_true", help="Clear the output directory, then build.")
+    build_cmd.add_argument(
+        "-d","--dry-run", action="store_true", help="Run as normal. but don't create build files.")
+    build_cmd.add_argument(
+        "-p", "--working-dir", type=Path, default=cwd,
+        help="Use the specified directory, instead of the current directory")
+
+    args = parser.parse_args(argv)
+    configure_logging(args.verbose)
+    match args.command:
+        case "build":
+            build(
+                force=args.force,
+                directory=args.working_dir,
+                perform_clean=args.clean,
+                dry_run=args.dry_run,
+            )
+        case _:
+            parser.print_help()
+
+
+
+def build(force: bool, directory: Path, perform_clean: bool, dry_run: bool) -> None:
     logger.info("Building site at %s.", directory)
     s_time = time.perf_counter()
     site = SiteRoot(directory)
@@ -60,4 +80,4 @@ def build(force: bool, directory: Path):
     logger.info(build_stats.summary(m))
 
 if __name__ == "__main__":
-    cli()
+    main()
